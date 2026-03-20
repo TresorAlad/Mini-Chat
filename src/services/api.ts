ed232f87e4e3e210a4aefba556b45998468ad196
It's Trésor <bernardalade92@gmail>
2026-03-20 23:56:25 +0000

    ouou

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:8080/api";
-const WS_URL = "ws://localhost:8080/ws";
+const API_URL = import.meta.env.VITE_API_URL;
+const WS_URL = import.meta.env.VITE_WS_URL;
 
 export async function registerUser(username: string, email: string, password: string) {
   const res = await fetch(`${API_URL}/register`, {
